a14dfd8f3274d24c7481f321bb0090c7577ecd29
Vikash Pandian <vikashpandian@gmail.com>
2022-03-03 21:42:04 -0500

    Made changes to the models of answers and comments

--- backend/src/models/models.go
+++ backend/src/models/models.go
@@ -24,19 +24,22 @@ type Question struct {
 
 type Answer struct {
 	Id          primitive.ObjectID `json:"id,omitempty"`
+	QuestionId	string			   `json:"question_id" validate:"required"`
 	Author      string             `json:"author" validate:"required"`
 	Body        string             `json:"body" validate:"required"`
-	Upvotes     int                `json:"upvotes" validate:"required"`
-	Downvotes   int                `json:"downvotes" validate:"required"`
-	CreatedTime time.Time          `json:"created_time" validate:"required"`
-	UpdatedTime time.Time          `json:"updated_time" validate:"required"`
-	Comments    []Comment          `json:"comments" validate:"required"`
+	Upvotes     int                `json:"upvotes,omitempty"`
+	Downvotes   int                `json:"downvotes,omitempty"`
+	CreatedTime time.Time          `json:"created_time,omitempty"`
+	UpdatedTime time.Time          `json:"updated_time,omitempty"`
+	Comments    []Comment          `json:"comments,omitempty" validate:"required"`
 }
 
 type Comment struct {
 	Id          primitive.ObjectID `json:"id,omitempty"`
+	QuestionId	string			   `json:"question_id" validate:"required"`
+	AnswerId	string			   `json:"answer_id,omitempty"`
 	Author      string             `json:"author" validate:"required"`
 	Body        string             `json:"body" validate:"required"`
-	CreatedTime time.Time          `json:"created_time" validate:"required"`
-	UpdatedTime time.Time          `json:"updated_time" validate:"required"`
+	CreatedTime time.Time          `json:"created_time,omitempty"`
+	UpdatedTime time.Time          `json:"updated_time,omitempty"`
 }
