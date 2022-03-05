a891984d82b8cb824dbb284eac9359040b76eda7
prasa_dd_vp <prasad.venkatesh90@gmail.com>
2022-03-04 20:53:48 -0500

    Integrated fe be to view questions, add answer and comment

--- front-end/src/components/QuestionView/MainQuestion.js
+++ front-end/src/components/QuestionView/MainQuestion.js
@@ -1,64 +1,142 @@
 import { Avatar } from '@mui/material';
-import React, { useState } from "react";
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
 import ReactQuill from 'react-quill';
 import "react-quill/dist/quill.snow.css";
 import { Link } from 'react-router-dom';
 import "./index.css";
+import ReactHtmlParser from 'react-html-parser';
 
 function MainQuestion() {
     const [show, setShow] = useState(false)
+    const [answer, setAnswer] = useState("")
+    const [comment, setComment] = useState("")
+    const [questionData, setQuestionData] = useState()
+    let search = window.location.search
+    const params = new URLSearchParams(search)
+    const id = params.get("q")
+
+    const handleQuill = (value) => {
+        setAnswer(value)
+    }
+
+    const handleSubmit = async() => {
+        if(answer !== "") {
+            const body = {
+                question_id: id,
+                body:  answer,
+                author: "default",
+                comments: []
+            }
+            const config = {
+                header: {
+                    "Content-type": "application/json"
+                }
+            }
+
+            await axios.post("/api/v2/answers", body, config).then((res) => {
+                console.log(res.data)
+                alert("Answer added successfully")
+                setAnswer("")
+                getUpdatedAnswer()
+            }).catch((err) => console.log(err))
+        }
+    }
+
+    const handleComment = async() => {
+        if (comment !== ""){
+            const body = {
+                question_id: id,
+                body: comment,
+                author: "default"
+            }
+
+            await axios.post("/api/v2/comments", body).then((res) => {
+                console.log(res.data)
+                setComment("")
+                setShow(false)
+                getUpdatedAnswer()
+            })
+        }
+    }
+    
+    async function getUpdatedAnswer() {
+        await axios.get(`api/v1/questions/${id}`).then((res) => {
+            console.log(res.data)
+            setQuestionData(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        async function getQuestionDetails() {
+            await axios.get(`api/v1/questions/${id}`).then((res) => {
+                console.log(res.data)
+                setQuestionData(res.data)
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+        getQuestionDetails()
+    }, [id])
+
     return (
         <div className="main"> 
            <div className="main-question-container">
-               <div class="main-top">
-                   <h2 className="main-question">React-router urls don't work when refreshing or writing manually</h2>
+               <div className="main-top">
+                   <h2 className="main-question">{questionData?.title}</h2>
                    <Link to='./ask-question'><button>Ask Question</button></Link>
                </div>
                <div className="main-desc">
                    <div className="info">
-                       <p>Timestamp</p>
-                       <p>Active<span>Today</span></p>
-                       <p>Viewed<span>10 times</span></p>
+                       <p>Asked<span>{new Date(questionData?.createdtime).toLocaleString()}</span></p>
+                       <p>Active<span>{new Date(questionData?.updatedtime).toLocaleString()}</span></p>
+                       <p>Viewed<span>{questionData?.views} times</span></p>
                    </div>
                </div>
                <div className="question-body">
                    <div className="question-body-container">
                        <div className="question-body-left">
                             <div className="all-options">
-                                <Link><p className="arrow">▲</p></Link>
-                                <p className="arrow">0</p>
-                                <Link><p className="arrow">▼</p></Link>
+                                <Link to="/"><p className="arrow">▲</p></Link>
+                                <p className="arrow">{questionData?.upvotes - questionData?.downvotes}</p>
+                                <Link to="/"><p className="arrow">▼</p></Link>
                            </div>
                        </div>
                        <div className="question-answer">
-                           <p>I'm using React-router and it works fine while I'm clicking on link buttons, but when I refresh my webpage it does not load what I want.
-
-For instance, I am in localhost/joblist and everything is fine because I arrived here pressing a link. But If I refresh the webpage I get:I'm using React-router and it works fine while I'm clicking on link buttons, but when I refresh my webpage it does not load what I want.
-
-For instance, I am in localhost/joblist and everything is fine because I arrived here pressing a link. But If I refresh the webpage I get:I'm using React-router and it works fine while I'm clicking on link buttons, but when I refresh my webpage it does not load what I want.
-
-For instance, I am in localhost/joblist and everything is fine because I arrived here pressing a link. But If I refresh the webpage I get:I'm using React-router and it works fine while I'm clicking on link buttons, but when I refresh my webpage it does not load what I want.
-
-For instance, I am in localhost/joblist and everything is fine because I arrived here pressing a link. But If I refresh the webpage I get:</p>
+                           <p>{ReactHtmlParser(questionData?.body)}</p>
                            <div className="author">
-                               <small>Asked "Timestamp"</small>
-                               <div className="author-details"><Avatar/><p>Author name</p></div>
+                               <small>Asked {new Date(questionData?.createdtime).toLocaleString()}</small>
+                               <div className="author-details"><Avatar/><p>{questionData?.author}</p></div>
                            </div>
                            <div className="comments">
                                <div className="comment">
-                                   <p>This is comment - <span>User name <small>Timestamp</small></span></p>
+                                    {
+                                        questionData?.comments && questionData?.comments?.map((comment) => <p>
+                                            {comment?.body} - <span>{comment?.author}</span> <small>{new Date(comment?.createdtime).toLocaleString()}</small>
+                                        </p>)
+                                    }
                                </div>
                                <p onClick={() => setShow(!show)}>Add a comment</p>
                                {show && (
                                     <div className="title">
-                                        <textarea type="text" placeholder="Add your comment..." style={{
+                                        <textarea 
+                                            value={comment}
+                                            onChange= {(e) => setComment(e.target.value)}
+                                            type="text" 
+                                            placeholder="Add your comment..." 
+                                            style={{
                                             margin: "5px 0px",
                                             padding: "10px",
                                             border: "1px solid rgba(0,0,0,0.2)",
                                             borderRadius: "3px",
                                             outline: "none"
                                         }} rows={5}></textarea>
-                                        <button style={{maxWidth:"fit-content"}}>Add comment</button>
+                                        <button 
+                                            onClick={handleComment}
+                                            style={{maxWidth:"fit-content"}}>Add comment
+                                        </button>
                                     </div>
                                )}
                            </div>
@@ -66,31 +144,37 @@ For instance, I am in localhost/joblist and everything is fine because I arrived
                    </div>
                </div>
                <div style={{flexDirection:"column"}} className="question-body">
-                   <p style={{marginBottom:"20px", fontSize:"1.3rem", fontWeight:"300"}}>1 Answer</p>
-                   <div className="question-body-container">
-                        <div className="question-body-left">
-                           <div className="all-options">
-                                <Link><p className="arrow">▲</p></Link>
-                                <p className="arrow">0</p>
-                                <Link><p className="arrow">▼</p></Link>
-                           </div>
-                       </div>
-                       <div className="question-answer">
-                           <p>I'm not sure if the reason why this choice was originally made is documented anywhere (although, for all I know, it could be explained in great length in some PEP somewhere), but we can certainly come up with various reasons why it makes sense.
-                              One reason is simply that rounding towards negative (or positive!) infinity means that all numbers get rounded the same way, whereas rounding towards zero makes zero special. The mathematical way of saying this is that rounding down towards is translation invariant, i.e. it satisfies the equation:</p>
-                           <div className="author">
-                               <small>Asked "Timestamp"</small>
-                               <div className="author-details"><Avatar/><p>Author name</p></div>
-                           </div>
+                   <p style={{marginBottom:"20px", fontSize:"1.3rem", fontWeight:"300"}}>{questionData?.answers?.length} Answer(s)</p>
+                    {
+                       questionData?.answers?.map((answer) => (
+                        <>
+                        <div key={answer?.id} className="question-body-container">
+                            <div className="question-body-left">
+                            <div className="all-options">
+                                    <Link to="/"><p className="arrow">▲</p></Link>
+                                    <p className="arrow">0</p>
+                                    <Link to="/"><p className="arrow">▼</p></Link>
+                            </div>
+                            </div>
+                            <div className="question-answer">
+                            <p>{ReactHtmlParser(answer?.body)}</p>
+                            <div className="author">
+                                <small>Asked {new Date(answer?.createdtime).toLocaleString()}</small>
+                                <div className="author-details"><Avatar/><p>{answer?.author}</p></div>
+                            </div>
+                            </div>
                         </div>
-                   </div>
+                        </>
+                        ))  
+                    }
+                   
                </div>
            </div>
            <div className="main-answer">
                <h3 style={{fontSize: "22px", margin:"10px 0", fontWeight:"400"}}>Your answer</h3>
-               <ReactQuill className="react-quill" theme="snow" style={{height: "200px"}}/>
+               <ReactQuill className="react-quill" theme="snow" style={{height: "200px"}} value={answer} onChange={handleQuill}/>
            </div>
-           <button style={{maxWidth:"fit-content", marginTop:"50px"}}>Post your answer</button>
+           <button type="submit" onClick={handleSubmit} style={{maxWidth:"fit-content", marginTop:"50px"}}>Post your answer</button>
         </div>
     );
 }
