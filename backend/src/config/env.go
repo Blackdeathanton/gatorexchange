87433474fa81c66176607a5652152869235ccb7a
Liju Anton <blackdeathanton@gmail.com>
2022-03-29 20:07:29 -0400

    Added a method to fetch JWTKEY property from .env

--- backend/src/config/env.go
+++ backend/src/config/env.go
@@ -16,3 +16,12 @@ func GetMongoURI() string {
 
 	return os.Getenv("MONGOURI")
 }
+
+func GetJWTKey() string {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("An error occurred while loading .env file", err)
+	}
+	return os.Getenv("JWTKEY")
+}
