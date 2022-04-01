48b3cea982391cd5a3267f3913d380eba12ba858
prasa_dd_vp <prasad.venkatesh90@gmail.com>
2022-03-31 22:08:55 -0400

    authenticating user for specific routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,35 @@ import Appbar from './components/Appbar/Appbar';
 import AllQuestionsView from './components/AllQuestionsView';
 import AskQuestion from './components/AskQuestion/AskQuestion';
 import Auth from './components/Auth';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import QuestionView from './components/QuestionView';
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+
+    const user = useSelector(selectUser);
+
+    const PrivateRoute = ({ component: Component, ...rest }) => (
+        <Route
+        {...rest}
+        render={(props) =>
+            sessionStorage.getItem("token") ? (
+            <Component {...props} />
+            ) : (
+            <Redirect
+                to={{
+                pathname: "/auth",
+                state: {
+                    from: props.location,
+                },
+                }}
+            />
+            )
+        }
+        />
+    );
+
     return (
         <div className="App">
             <Router>
@@ -14,10 +39,10 @@ function App() {
                 <Switch>
                     <Route exact path='/auth' component = {Auth}/>
                     <Route exact path='/' component = {AllQuestionsView}/>
-                    <Route exact path='/:id' component = {AllQuestionsView}/>
+                    {/* <Route exact path='/:id' component = {AllQuestionsView}/> */}
                     <Route exact path='/question' component = {QuestionView}/>
                     <Route exact path='/question/:id' component = {QuestionView}/>
-                    <Route exact path='/ask-question' component = {AskQuestion}/>
+                    <PrivateRoute exact path='/ask-question' component = {AskQuestion}/>
                 </Switch>
             </Router>
         </div>
