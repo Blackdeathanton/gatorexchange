e6df4057020983663b6efc0bdccf1ade3e173293
prasa_dd_vp <prasad.venkatesh90@gmail.com>
2022-04-01 20:13:02 -0400

    Handled filter by tags

--- front-end/src/components/AllQuestionsView/AllQuestions.js
+++ front-end/src/components/AllQuestionsView/AllQuestions.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import FilterListIcon from "@material-ui/icons/FilterList";
 import QuestionCard from './QuestionCard';
 import "./css/AllQuestions.css";
+import {TagsInput} from 'react-tag-input-component';
+import "@pathofdev/react-tag-input/build/index.css";
+import { useHistory } from 'react-router-dom';
 
 export default function AllQuestions({questions}) {
+
+    const [showFilters, setFiltersVisibility] = useState(false);
+    const history = useHistory();
+    
+    const [noAnswer, setNoAnswer] = useState(false);
+    const [hasUpvotes, setHasUpvotes] = useState(false);
+
+    const [sort, setSort] = useState("recent");
+
+    const [tags, setTags] = useState([])
+
+    const toggleFilterDisplay = () => {
+        setFiltersVisibility(!showFilters)
+    }
+
+
+    const handleApplyFilter = () => {
+        let url = "/questions";
+        if (tags !== []){
+            url += "?tag=" + tags.join(' ')
+        }
+        history.push(encodeURI(url))
+    }
+
     return (
         <div className="all-questions-view">
             <div className="all-questions-view-container">
@@ -15,7 +43,84 @@ export default function AllQuestions({questions}) {
                 </div>
                 <div className="all-questions-view-options">
                     <p>{questions && questions.length} Questions</p>
+                    <div className="main-filter">
+                        {/* <div className="main-tabs">
+                            <div className="main-tab">
+                                <Link to="/questions?q=recent">Recent</Link>
+                            </div>
+                            <div className="main-tab">
+                                <Link to="/questions?q=views">Views</Link>
+                            </div>
+                            <div className="main-tab">
+                                <Link to="/questions?q=upvotes">Upvotes</Link>
+                            </div>
+                        </div> */}
+                        <div className="main-filter-item" onClick={toggleFilterDisplay}>
+                            <FilterListIcon />
+                            <p>Filter</p>
+                        </div>
+                    </div>
                 </div>
+
+                { showFilters ? (
+                    <>
+                        <div className="filter-options-main-container">
+                            <div className="filters-container">
+                                <div className="filter-container">
+                                    <legend>Filter by</legend>
+                                    <div className="filter-items">
+                                        <div className="filter-item">
+                                            <input type="checkbox" checked={noAnswer} onChange={(e) => setNoAnswer(e.target.checked)}/>
+                                            <p>No answers</p>
+                                        </div>
+                                        <div className="filter-item">
+                                            <input type="checkbox" checked={hasUpvotes} onChange={(e) => setHasUpvotes(e.target.checked)}/>
+                                            <p>Has upvotes</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="filter-container">
+                                    <legend>Sort by</legend>
+                                    <div className="filter-item">
+                                        <input type="radio" value="recent" checked={sort==="recent"} onChange={(e) => setSort(e.target.value)}/>
+                                        <p>Recent</p>
+                                    </div>
+                                    <div className="filter-item">
+                                        <input type="radio" value="views" checked={sort==="views"} onChange={(e) => setSort(e.target.value)}/>
+                                        <p>Views</p>
+                                    </div>
+                                    <div className="filter-item">
+                                        <input type="radio" value="upvotes" checked={sort==="upvotes"} onChange={(e) => setSort(e.target.value)}/>
+                                        <p>Upvotes</p>
+                                    </div>
+                                </div>
+                                <div className="filter-container">
+                                    <legend>Tagged with</legend>
+                                    <div className="filter-tag-container">
+                                        <TagsInput 
+                                            value={tags}
+                                            onChange={setTags}
+                                            name="tags" 
+                                            placeHolder="Eg., Python"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="filters-footer">
+                                <button type="submit" onClick={handleApplyFilter} className="filters-button button">Apply filter</button>
+                                <p className="filters-button" onClick={toggleFilterDisplay}> Cancel </p>
+                            </div>
+                        </div>
+                    </>
+                ) :
+                ( 
+                    <>
+                    
+                    </>
+                )
+                } 
+                
                 <div className="all-questions-view-content">{
                     questions.map((question, index) => (
                         <div className="question-view-content" key={index}>
--- front-end/src/components/AllQuestionsView/index.js
+++ front-end/src/components/AllQuestionsView/index.js
@@ -10,42 +10,103 @@ export default function Index() {
     let search = window.location.search
     const params = new URLSearchParams(search)
     const id = params.get("q")
+    const tags = params.get("tag")
 
     useEffect(() => {
         async function getQuestion() {
-            if(id?.length>0){
-                if(id[0] === '_') {
-                    await axios
-                        .get(`/api/v3/questions/tagged/${id.substring(1)}`)
-                        .then((res) => {
-                            setQuestions(res.data.reverse());
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                } else {
-                    await axios
-                        .get(`/api/v2/search?q=${id}`)
-                        .then((res) => {
-                            setQuestions(res.data.reverse());
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                }
+            if (id){
+                await axios
+                    .get(`/api/v2/search?q=${id}`)
+                    .then((res) => {
+                        setQuestions(res.data);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            } else if (tags){
+                await axios
+                    .get(`/api/v3/questions/tagged/${tags}`)
+                    .then((res) => {
+                        res.data !== null ? setQuestions(res.data) : setQuestions([]);
+                        
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
             } else {
                 await axios
                 .get("/api/v1/questions")
                 .then((res) => {
-                    setQuestions(res.data.reverse());
+                    setQuestions(res.data);
                 })
                 .catch((err) => {
                     console.log(err);
                 });
             }
+
+
+
+
+            // if(id?.length>0){
+            //     if(id[0] === '_') {
+            //         await axios
+            //             .get(`/api/v3/questions/tagged/${id.substring(1)}`)
+            //             .then((res) => {
+            //                 setQuestions(res.data);
+            //             })
+            //             .catch((err) => {
+            //                 console.log(err);
+            //             });
+            //     } 
+
+            //     // else if(id === "recent" || id === "views" || id === "upvotes") {
+            //     //     await axios
+            //     //         .get(`/api/v1/questions?sort=${id}`)
+            //     //         .then((res) => {
+            //     //             setQuestions(res.data);
+            //     //         })
+            //     //         .catch((err) => {
+            //     //             console.log(err);
+            //     //         });
+                    
+            //     // } 
+                
+            //     else {
+            //         await axios
+            //             .get(`/api/v2/search?q=${id}`)
+            //             .then((res) => {
+            //                 setQuestions(res.data);
+            //             })
+            //             .catch((err) => {
+            //                 console.log(err);
+            //             });
+            //     }
+            // } 
+            
+            // else if (tags){
+            //     await axios
+            //         .get(`/api/v3/questions/tagged/${tags}`)
+            //         .then((res) => {
+            //             setQuestions(res.data);
+            //         })
+            //         .catch((err) => {
+            //             console.log(err);
+            //         });
+            // }
+            
+            // else {
+            //     await axios
+            //     .get("/api/v1/questions")
+            //     .then((res) => {
+            //         setQuestions(res.data);
+            //     })
+            //     .catch((err) => {
+            //         console.log(err);
+            //     });
+            // }
         }
         getQuestion();
-    }, [id]);
+    }, [id, tags]);
 
     return (
         <div className="all-questions-view-main">
