2401cc0fae60f32232e65e577c9d50264d4f1b17
prasa_dd_vp <prasad.venkatesh90@gmail.com>
2022-04-19 00:44:26 -0400

    Added get user details api call

--- front-end/src/components/UserProfile/index.js
+++ front-end/src/components/UserProfile/index.js
@@ -1,7 +1,28 @@
 import React from "react";
 import Sidebar from '../Sidebar/Sidebar';
+import axios from "axios";
 
 export default function UserProfile() {
+
+    let search = window.location.search
+    const params = new URLSearchParams(search)
+    const name = params.get("q")
+    const [userDetails, setUserDetails] = useState({});
+
+    useEffect(() => {
+        async function getUserDetails() {
+            await axios
+                .get(`/api/v4/users/${name}`)
+                .then((res) => {
+                    console.log(res.data);
+                    setUserDetails(res.data[0]);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+        getUserDetails();
+    }, [name]);
 
     return (
         <div className="user-profile-main">
