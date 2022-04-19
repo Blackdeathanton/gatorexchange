5ac68af6995ddfcd2a194b66f4660b5e0de165e2
prasa_dd_vp <prasad.venkatesh90@gmail.com>
2022-04-19 00:45:09 -0400

    Added author, stats and tags section

--- front-end/src/components/UserProfile/index.js
+++ front-end/src/components/UserProfile/index.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './css/index.css';
 import Sidebar from '../Sidebar/Sidebar';
 import axios from "axios";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default function UserProfile() {
 
@@ -28,6 +31,62 @@ export default function UserProfile() {
         <div className="user-profile-main">
             <div className="user-profile-content">
                 <Sidebar/>
+                <div className="user-profile-view">
+                    <div className="user-profile-container">
+                        
+                        {/* top section */}
+                        <div className="user-profile-top">
+                            <div className="user-avatar">
+                                <Avatar style={{
+                                            width: "120px",
+                                            height: "120px"
+                                        }}/>
+                            </div>
+                            <div className="user-details">
+                                <h2>{userDetails?.username}</h2>
+                                <p>{userDetails?.firstname+' '+userDetails?.lastname}</p>
+                                <p>{userDetails?.email}</p>
+                                <p>Member since {userDetails?.createdtime?.substring(0,10)}</p>
+                            </div>
+                        </div>
+
+                        {/* Stats section */}
+                        <div className="user-profile-stats">
+                            <h2>Stats</h2>
+                            <div className="stats-container">
+                                <div className="stats-content">
+                                    <div className="stats-item">
+                                        <div>{userDetails?.questions?.length}</div>
+                                        <p>Questions</p>
+                                    </div>
+                                    <div className="stats-item">
+                                        <div>{userDetails?.answers?.length}</div>
+                                        <p>Answers</p>
+                                    </div>
+                                    <div className="stats-item">
+                                        <div>{userDetails?.tags?.length}</div>
+                                        <p>Tags</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Tags Section */}
+                        <div className="user-profile-tags">
+                            <h2>Tags</h2>
+                            <div className="tags-container">
+                                <div className="tags-content">
+                                    {    
+                                        userDetails?.tags?.map((tag, index) => (
+                                        <div className="tag-item" key={index}>
+                                            <Link to={`/questions?tag=${encodeURIComponent(tag._id)}`}><div className="tagname">{tag._id}</div></Link>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
